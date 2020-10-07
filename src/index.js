6f63429dd1454ce76f291fd632f4031f4344d878
Martin-Sparks <martin.sparks82@gmail.com>
2020-10-07 15:56:44 +0100

    basic react game engine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React, { PureComponent } from "react";
+// import ReactDOM from 'react-dom'
+import { GameEngine } from "react-game-engine";
+import { Box } from "./components/renderers";
+import { MoveBox } from "./components/systems";
+ 
+export default class SimpleGame extends PureComponent {
+  render() {
+    return (
+      <GameEngine
+        style={{ width: 800, height: 600, backgroundColor: "blue" }}
+        systems={[MoveBox]}
+        entities={{
+          //-- Notice that each entity has a unique id (required)
+          //-- and a renderer property (optional). If no renderer
+          //-- is supplied with the entity - it won't get displayed.
+          box1: { x: 200,  y: 200, renderer: <Box />}
+        }}>
+ 
+      </GameEngine>
+    );
+  }
+}
